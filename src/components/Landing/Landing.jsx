496516b0472c255f802928a19cb92d665286c811
chris023 <chris.meyer023@gmail.com>
2019-02-05 12:16:10 -0700

    Fetch opening crawl from swapi

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+import './Landing.scss';
+
 export default class Landing extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      openingCrawl: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/films/1/')
+      .then((response) => response.json())
+      .then(({ opening_crawl }) =>
+        this.setState({
+          openingCrawl: opening_crawl
+        })
+      )
+  }
+
   render() {
+    const { openingCrawl } = this.state;
+
     return (
-      <p>Landing</p>
+      <div className='Landing'>
+        <p>{ openingCrawl }</p>
+      </div>
     )
   }
 }
