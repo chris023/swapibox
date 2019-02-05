fb127d7468724a334dbb5d7970bd0876c95238e6
chris023 <chris.meyer023@gmail.com>
2019-02-05 11:50:47 -0700

    Center nav buttons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import './Sidebar.scss';
 
 export default class Sidebar extends Component {
   render() {
     return (
-      <p>Sidebar</p>
+      <div className='Sidebar'>
+        <button>People</button>
+        <button>Planets</button>
+        <button>Vehicles</button>
+        <button>Favorites</button>
+        <p className='text'></p>
+      </div>
     )
   }
 }
