8acbd7e769d027949bb9bdf5fc04e3da053d18e7
chris023 <chris.meyer023@gmail.com>
2019-02-05 14:12:15 -0700

    Add movie-info and loading text

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './Landing.scss';
 
@@ -8,6 +8,9 @@ export default class Landing extends Component {
     super(props);
     this.state = {
       openingCrawl: '',
+      title: '',
+      releaseYear: '',
+      loading: true,
     }
   }
 
@@ -16,20 +19,43 @@ export default class Landing extends Component {
       .then((response) => response.json())
       .then(({ results, count }) => {
         const randIndex = Math.round(Math.random() * (count - 1));
-        const { opening_crawl } = results[randIndex];
+        const { opening_crawl, title, release_date } = results[randIndex];
         
         this.setState({
-          openingCrawl: opening_crawl
+          openingCrawl: opening_crawl,
+          title,
+          releaseYear: release_date.split('-')[0],
+          loading: false,
         })
       })
   }
 
   render() {
-    const { openingCrawl } = this.state;
+    const {
+      openingCrawl,
+      title,
+      releaseYear,
+      loading,
+    } = this.state;
 
     return (
       <div className='Landing'>
-        <p>{ openingCrawl }</p>
+        {
+          loading
+            ? (
+              <p>Loading...</p>
+            )
+            : (
+              <Fragment>
+                <p className='opening-crawl'>{openingCrawl}</p>
+                <div className='movie-info'>
+                  <p>{title}</p>
+                  <p>({releaseYear})</p>
+                </div>
+              </Fragment>
+            ) 
+        }
+        
       </div>
     )
   }
