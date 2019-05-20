103cfd917bc5f64cace0c78f61bc77a46b66fc92
chris023 <chris.meyer023@gmail.com>
2019-05-20 12:41:46 -0600

    Updates

---
+++ precache-manifest.1bc878cb44b473bbf0ff96385b350e93.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "9337f381e7a52848508f3fa125a31dc1",
+    "url": "/swapibox/static/media/luke.9337f381.png"
+  },
+  {
+    "revision": "df0cfa7adda9a68c7958",
+    "url": "/swapibox/static/css/main.d37b3521.chunk.css"
+  },
+  {
+    "revision": "1bd58d977989d815a8d9",
+    "url": "/swapibox/static/js/1.1bd58d97.chunk.js"
+  },
+  {
+    "revision": "c9f58f50f745c5d4961b",
+    "url": "/swapibox/static/js/runtime~main.c9f58f50.js"
+  },
+  {
+    "revision": "888705afaae26795a9cdea0a1b36680f",
+    "url": "/swapibox/static/media/darthvader.888705af.png"
+  },
+  {
+    "revision": "ce8f9a3c28af9e53029cc67e8a4beb74",
+    "url": "/swapibox/static/media/alderaan.ce8f9a3c.png"
+  },
+  {
+    "revision": "d44a7f56c2791f5b99ade1ae9ca4fc06",
+    "url": "/swapibox/static/media/atat.d44a7f56.png"
+  },
+  {
+    "revision": "7ce8ba0215ac2e38ee66726bfb776151",
+    "url": "/swapibox/static/media/atst.7ce8ba02.png"
+  },
+  {
+    "revision": "01178ef9d53db60bb728f877e634f441",
+    "url": "/swapibox/static/media/beru.01178ef9.png"
+  },
+  {
+    "revision": "553aab71dc195b2864592a3c9b16df51",
+    "url": "/swapibox/static/media/bespin.553aab71.png"
+  },
+  {
+    "revision": "96d099ddf46f374b561046bd45ac4499",
+    "url": "/swapibox/static/media/bigg.96d099dd.png"
+  },
+  {
+    "revision": "1f31c7c2bf8de92463b5a0a425bbdb26",
+    "url": "/swapibox/static/media/c3p0.1f31c7c2.png"
+  },
+  {
+    "revision": "0a98859b8fc6571abd6845d861a2531d",
+    "url": "/swapibox/static/media/cloudcar.0a98859b.png"
+  },
+  {
+    "revision": "9aaaef0fa1b1c314a7a987e3e42d1a13",
+    "url": "/swapibox/static/media/coruscant.9aaaef0f.png"
+  },
+  {
+    "revision": "fe6ec7e89f442e8186f812f7f494e933",
+    "url": "/swapibox/static/media/dagobah.fe6ec7e8.png"
+  },
+  {
+    "revision": "1ca16ea654b8077b0fbd7255b8342442",
+    "url": "/swapibox/static/media/endor.1ca16ea6.png"
+  },
+  {
+    "revision": "bcdeaf372eb7c5037835264f31fa3311",
+    "url": "/swapibox/static/media/geonosis.bcdeaf37.png"
+  },
+  {
+    "revision": "862f0010e7d024c50d12275c219438df",
+    "url": "/swapibox/static/media/hoth.862f0010.png"
+  },
+  {
+    "revision": "b368f2de30c9d9bae055d75f9f9c5246",
+    "url": "/swapibox/static/media/kamino.b368f2de.png"
+  },
+  {
+    "revision": "ed2be78385c04f3dcff722f8bf2fa5e2",
+    "url": "/swapibox/static/media/leia.ed2be783.png"
+  },
+  {
+    "revision": "df0cfa7adda9a68c7958",
+    "url": "/swapibox/static/js/main.df0cfa7a.chunk.js"
+  },
+  {
+    "revision": "e6ed8eb0a307e0c8552fbeacc2afbafe",
+    "url": "/swapibox/static/media/naboo.e6ed8eb0.png"
+  },
+  {
+    "revision": "59fc7c6dc89b0a217bb893ee6029c515",
+    "url": "/swapibox/static/media/obiwan.59fc7c6d.png"
+  },
+  {
+    "revision": "c82800f190aca768cd737fbfb722acd0",
+    "url": "/swapibox/static/media/owen.c82800f1.png"
+  },
+  {
+    "revision": "fc13d8c106d2f4cc7d7522f4bf4f1a10",
+    "url": "/swapibox/static/media/r2d2.fc13d8c1.png"
+  },
+  {
+    "revision": "97a92a26fee69670edd377ac9678541d",
+    "url": "/swapibox/static/media/r5d4.97a92a26.png"
+  },
+  {
+    "revision": "beec568783cbb28b0911340b8911cb8a",
+    "url": "/swapibox/static/media/sailbarge.beec5687.png"
+  },
+  {
+    "revision": "1982d71668349a8db9de15d4581eaff5",
+    "url": "/swapibox/static/media/sandcrawler.1982d716.png"
+  },
+  {
+    "revision": "4859b0b09aa487110ecbc8bd15109d3d",
+    "url": "/swapibox/static/media/snowspeeder.4859b0b0.png"
+  },
+  {
+    "revision": "b0b5fd7a86c8d0817724bb432547e1cc",
+    "url": "/swapibox/static/media/t16skyhopper.b0b5fd7a.png"
+  },
+  {
+    "revision": "c18c1e90d19092be78e6ffe0053936d3",
+    "url": "/swapibox/static/media/tiebomber.c18c1e90.png"
+  },
+  {
+    "revision": "f40e9d6ac5243652daa4589ddc74ddcc",
+    "url": "/swapibox/static/media/tiefighter.f40e9d6a.png"
+  },
+  {
+    "revision": "4343635312350749d245826e93c06544",
+    "url": "/swapibox/static/media/x34landspeeder.43436353.png"
+  },
+  {
+    "revision": "4a60ddf59b20ca3848f7bc216eac3c8a",
+    "url": "/swapibox/static/media/yavin4.4a60ddf5.png"
+  },
+  {
+    "revision": "47e05de7dab52f157a92836a4f38296e",
+    "url": "/swapibox/static/media/refresh.47e05de7.png"
+  },
+  {
+    "revision": "b90db333139efd4e4a7551b06c566e9d",
+    "url": "/swapibox/static/media/star.b90db333.png"
+  },
+  {
+    "revision": "75e7e25f23857d69d7688b0884cd0f56",
+    "url": "/swapibox/static/media/icons.75e7e25f.png"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/swapibox/static/media/Death Star.ade91cb0.otf"
+  },
+  {
+    "revision": "f353f1a017760286a7ede9c6288fb886",
+    "url": "/swapibox/static/media/stars.f353f1a0.jpg"
+  },
+  {
+    "revision": "a12a086cd932b3f5ff6f05c8c31c323a",
+    "url": "/swapibox/index.html"
+  }
+];
