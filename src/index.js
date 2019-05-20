7d0e0525ecfe266d44e2f2eaefa72c6a4924c9ea
chris023 <chris.meyer023@gmail.com>
2019-05-20 13:06:31 -0600

    Fix router basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/swapibox">
+  <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
